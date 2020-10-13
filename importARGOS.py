cbb8f70c031f65a16af10cb3c231138b5e459291
hzewdie <hz196@duke.edu>
2020-10-13 17:44:04 -0400

    Create point object and insert feature
    feature is not projected, need to fix

--- importARGOS.py
+++ importARGOS.py
@@ -13,10 +13,27 @@
 # Import modules
 import sys, os, arcpy
 
+# Allow arcpy to overwrite oupputs
+arcpy.env.overwriteOutput = True
+
 # Set input variables (Hard-wired)
 inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
 outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
+outputSR = arcpy.SpatialReference(54002)
 
+#create an empty feature class to which we will add features 
+outPath, outName =os.path.split(outputFC)
+arcpy.CreateFeatureclass_management(outPath,outName, "POINT",'','','',outputSR)
+
+#add field to our new feature class
+
+# Add TagID, LC, IQ, and Date fields to the output feature class
+arcpy.AddField_management(outputFC,"TagID","LONG")
+arcpy.AddField_management(outputFC,"LC","TEXT")
+arcpy.AddField_management(outputFC,"Date","DATE")
+
+# create an inset cursor: enable me to access files 
+cur = arcpy.da.InsertCursor(outputFC,["Shape@", "TagID", "LC", "Date"])
 #%% Construct a while loop to iterate through all lines in the datafile
 #open the ARGOS datafile for reading
 
@@ -49,11 +66,35 @@ while lineString:
         obsLat = line2Data[2]
         obsLon= line2Data[5]
         
+        try:
+              # Convert raw coordinate strings to numbers
+            if obsLat[-1] == 'N':
+                obsLat = float(obsLat[:-1])
+            else:
+                obsLat = float(obsLat[:-1]) * -1
+            if obsLon[-1] == 'E':
+                obsLon = float(obsLon[:-1])
+            else:
+                obsLon = float(obsLon[:-1]) * -1
+            
+            #Create a point object 
+            obsPoint = arcpy.Point()
+            obsPoint.X = obsLon
+            obsPoint.Y = obsLat
+            
+        except Exception as e:
+            print(f"Error adding record {tagID} to the output")
+            
+        # add a feature using our insert cursor
+        feature = cur.insertRow((obsPoint,tagID,LC,date.replace(".","/") + " " + time))
         # Print results to see how we're doing
-        print (tagID, date,time,LC,"Lat:"+obsLat,"Long:"+obsLon)
+        #print (tagID, date,time,LC,"Lat:"+obsLat,"Long:"+obsLon)
         
     # Move to the next line so the while loop progresses
     lineString = inputFileObj.readline()
     
 #Close the file object
 inputFileObj.close()
+
+#delete the cursor 
+del cur
